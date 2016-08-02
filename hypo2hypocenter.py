1d98f7563217348b33520199efda5a81d427cbde
Tiago Gomes da Silva <tiagogomes.mb@gmail.com>
2016-08-01 23:54:32 -0400

    Update hypo2hypocenter.py

--- hypo2hypocenter.py
+++ hypo2hypocenter.py
@@ -3,7 +3,7 @@ import sys
 
 #S5  EP 1 980216073932.28       123.5 S 3
 #0123456789012345678901234567890123456789
-f = open('hypo71_datas.txt','r')
+f = open('dados_mestrado.txt','r')
 s = open('hypocenter', 'w')
 lines  = f.readlines()
 for line in lines:
@@ -24,21 +24,22 @@ for line in lines:
 	resto_tempo_s = int(tempo_s) % 60
 	# Calcular o tempo da S 
 	timeList = ['%2s:%2s:%2s:%s0' %(h,minuto,seg,ms) ,'0:%01s:%01s:%s00' %(tempo_s_minuto, resto_tempo_s, mili_da_s)] 
+	print(timeList)
 	sum = datetime.timedelta()
 	for i in timeList:
 		(h, m, s, ms) = i.split(':')
 		d = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s), milliseconds=int(ms))
 		sum += d
-		#print('' + str(sum))
+		print('' + str(sum))
 	h_cal, minuto_cal, segmili = str(sum).split(':')
 	h_cal = h_cal.zfill(2)
 	minuto_cal = minuto_cal.zfill(2)
-	segundo_cal, mili_cal = segmili.split('.') 
-	segundo_cal = segundo_cal.zfill(2)
-	mili_cal = mili_cal[0:3]
-	hora = ('%s%s%s.%s'%(h_cal, minuto_cal, segundo_cal, mili_cal))
+	#segundo_cal, mili_cal = segmili.split('.') 
+	#segundo_cal = segundo_cal.zfill(2)
+	#mili_cal = mili_cal[0:3]
+	hora = ('%s%s%s'%(h_cal, minuto_cal, segmili))
 	#print(hora)
-	print(' %4s SZ %2s   %s C %s0\n %s SZ %sS   %s C %s\n' %(estacao,pick_P, peso_P, t0, estacao, pick_S, peso_S, hora)) 
+	print(' %4s SZ %2s   %s C %s0\n %s SZ %sS   %s C %.10s\n' %(estacao,pick_P, peso_P, t0, estacao, pick_S, peso_S, hora)) 
 	
 
 # Abra o arquivo (leitura)
@@ -47,7 +48,7 @@ for line in lines:
 
 # insira seu conteudo
 # obs: o metodo append() e proveniente de uma lista
-	conteudo.append(' %s SZ %s   %s C %s0\n %s SZ %sS   %s C %s\n' 
+	conteudo.append(' %s SZ %s   %s C %s0\n %s SZ %sS   %s C %.10s\n' 
 	%(estacao,pick_P, peso_P, t0, estacao, pick_S, peso_S, hora)) 
 
 # Abre novamente o arquivo (escrita)
